2ec1f6cd1ba6d3fd60ce576e794f42fe97699b1a
Gloria Macia <gloria.macia@roche.com>
2021-12-05 21:33:34 +0100

    web mapp with catalonia counties added

--- mapa-web/app.py
+++ mapa-web/app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # https://stackoverflow.com/questions/37379374/insert-the-folium-maps-into-the-jinja-template
+    catalunya_comarques = "../amb-capital/data/catalunya_comarques.geojson"
     start_coords = (41.3874, 2.1686)
-    folium_map = folium.Map(location=start_coords, zoom_start=14, width="100%", height="100%")
-    folium_map.save('templates/map.html')
+    folium_map = folium.Map(location=start_coords, zoom_start=8, width="100%", height="100%")
+    folium.GeoJson(catalunya_comarques, name="catalunya_comarques").add_to(folium_map)
     return render_template('index.html', folium_map=folium_map._repr_html_())
 
 
