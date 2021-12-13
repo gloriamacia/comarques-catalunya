bb04da7ab44caa68c5d16fe94dc0598bab7ed59c
Gloria Macia <gloria.macia@roche.com>
2021-12-13 18:34:22 +0100

    add provinces as layers

--- mapa-web/app.py
+++ mapa-web/app.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # https://stackoverflow.com/questions/37379374/insert-the-folium-maps-into-the-jinja-template
-    catalunya_comarques = "../amb-capital/data/catalunya_comarques.geojson"
     start_coords = (41.3874, 2.1686)
     folium_map = folium.Map(location=start_coords, zoom_start=8, width="100%", height="100%")
-    folium.GeoJson(catalunya_comarques, name="catalunya_comarques").add_to(folium_map)
+    provincies = ["Barcelona", "Girona", "Lleida", "Tarragona"]
+    for provincia in provincies: 
+        geogson = f"../amb-capital/data/{provincia.lower()}_comarques.geojson"
+        folium.GeoJson(geogson, name=f"{provincia}").add_to(folium_map)
+    folium.LayerControl().add_to(folium_map)
     return render_template('index.html', folium_map=folium_map._repr_html_())
 
 
