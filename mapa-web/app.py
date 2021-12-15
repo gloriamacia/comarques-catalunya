9f8093f0cf2edbf7830116a4a91771b8b2b0a0a3
Gloria Macia <gloria.macia@roche.com>
2021-12-15 09:07:35 +0100

    add FeatureGroup to folium_map

--- mapa-web/app.py
+++ mapa-web/app.py
@@ -11,7 +11,13 @@ def index():
     provincies = ["Barcelona", "Girona", "Lleida", "Tarragona"]
     for provincia in provincies: 
         geogson = f"../amb-capital/data/{provincia.lower()}_comarques.geojson"
-        folium.GeoJson(geogson, name=f"{provincia}").add_to(folium_map)
+        if provincia == "Girona":
+            group = folium.FeatureGroup(name=f"{provincia}") 
+            folium.GeoJson(geogson).add_to(group)
+            folium.Marker(location=[41.8129, 2.9071], icon=folium.Icon(color="red", icon="plus", prefix='fa')).add_to(group)
+            group.add_to(folium_map)
+        else:
+            folium.GeoJson(geogson, name=f"{provincia}").add_to(folium_map)
     folium.LayerControl().add_to(folium_map)
     return render_template('index.html', folium_map=folium_map._repr_html_())
 
