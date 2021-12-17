ab7fad87fa970dac953fca35ef05fc55d6abfd28
Gloria Macia <gloria.macia@roche.com>
2021-12-17 17:59:23 +0100

    add pharmacies

--- mapa-web/app.py
+++ mapa-web/app.py
@@ -9,16 +9,15 @@ def index():
     # https://stackoverflow.com/questions/37379374/insert-the-folium-maps-into-the-jinja-template
     start_coords = (41.3874, 2.1686)
     folium_map = folium.Map(location=start_coords, zoom_start=8, width="100%", height="100%")
-    df = pd.read_csv('../catsalut/farmacies_catalunya.csv')
+    df = pd.read_csv('../catsalut/farmacies_geocoded.csv')
     df.dropna(subset =['lat', 'lng'], inplace=True)
-    provincies = ["Girona"] #, "Girona", "Lleida", "Tarragona"]
+    provincies = ["Barcelona", "Girona", "Lleida", "Tarragona"]
     for provincia in provincies: 
         geogson = f"../amb-capital/data/{provincia.lower()}_comarques.geojson"
         group = folium.FeatureGroup(name=f"{provincia}") 
         folium.GeoJson(geogson).add_to(group)
         farmacies = df[df['província']==provincia]
         farmacies.apply(lambda row: folium.Marker(location=[row['lat'], row['lng']], popup=row['Codi farm'], icon=folium.Icon(color="red", icon="plus", prefix='fa')).add_to(group), axis=1)
-        # folium.Marker(location=[41.8129, 2.9071], icon=folium.Icon(color="red", icon="plus", prefix='fa')).add_to(group)
         group.add_to(folium_map)
     folium.LayerControl().add_to(folium_map)
     return render_template('index.html', folium_map=folium_map._repr_html_())
