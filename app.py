392c81dde862b8c147e1e428860b17c9fa69c1e1
Gloria Macia <gloria.macia@roche.com>
2021-12-18 10:37:30 +0100

    change project structure

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ def index():
     # https://stackoverflow.com/questions/37379374/insert-the-folium-maps-into-the-jinja-template
     start_coords = (41.3874, 2.1686)
     folium_map = folium.Map(location=start_coords, zoom_start=8, width="100%", height="100%", prefer_canvas=True)
-    df = pd.read_csv('../catsalut/farmacies_geocoded.csv')
+    df = pd.read_csv('catsalut/farmacies_geocoded.csv')
     df.dropna(subset =['lat', 'lng'], inplace=True)
     provincies = ["Barcelona", "Girona", "Lleida", "Tarragona"]
     for provincia in provincies: 
-        geogson = f"../amb-capital/data/{provincia.lower()}_comarques.geojson"
+        geogson = f"amb-capital/data/{provincia.lower()}_comarques.geojson"
         group = folium.FeatureGroup(name=f"{provincia}") 
         folium.GeoJson(geogson).add_to(group)
         farmacies = df[df['província']==provincia]
@@ -22,5 +22,5 @@ def index():
     folium.LayerControl().add_to(folium_map)
     return render_template('index.html', folium_map=folium_map._repr_html_())
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
